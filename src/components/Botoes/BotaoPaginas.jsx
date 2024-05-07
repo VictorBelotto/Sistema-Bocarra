736d50490234c294da200ffd0254428684563e70
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2024-05-07 11:11:57 -0300

    add icons sideBar

--- src/components/Botoes/BotaoPaginas.jsx
+++ src/components/Botoes/BotaoPaginas.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const BotaoPaginas = ({ to, label, icon }) => {
   return (
-    <Link to={to} className='flex gap-2 w-4/5 mt-2 text-left text-white py-2 px-4 bg-purple-700 bg-opacity-0 hover:bg-purple-800 rounded-2xl'>
+    <Link to={to} className='flex gap-2 w-full py-2 px-4 text-base text-slate-300 text-left  hover:bg-purple-700 rounded-2xl'>
       {icon}{label}
     </Link>
   );
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -7,12 +7,14 @@ import {
   Accordion,
   AccordionHeader,
   AccordionBody,
+  ListItemPrefix,
 } from "@material-tailwind/react";
-
 import { ChevronRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
 import BotaoMenu from "../Botoes/BotaoMenu";
-import { Cone, Landmark, Pyramid } from "lucide-react";
 import {ViewAtivaContext} from '../../context/ViewAtiva.jsx'
+import BotaoPaginas from "../Botoes/BotaoPaginas.jsx";
+
+import { Calculator, Layers, LayoutList, ClipboardPenLine    } from 'lucide-react';
  
 export function SideBar() {
   const [open, setOpen] = React.useState(0);
@@ -40,7 +42,10 @@ export function SideBar() {
           }
         >
           <ListItem className="p-0" selected={open === 1}>
-            <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-2  hover:bg-card-escuro  rounded-2xl">
+            <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-2  hover:bg-card-escuro  rounded-2xl gap-2">
+              <ListItemPrefix>
+                <Layers  className="w-5 h-5"/>
+              </ListItemPrefix>
               <Typography color="blue-gray" className="mr-auto font-normal text-lg">
                 Orçamentos
               </Typography>
@@ -52,16 +57,17 @@ export function SideBar() {
 
               <ListItem className={`p-0 w-full rounded-2xl ${viewAtiva === 'calculadoraLona'? 'bg-purple-700': ''}`}>
                 <BotaoMenu 
+                  icon={<ChevronRightIcon strokeWidth={3} className="h-3 w-5 self-center" />} 
                   label={'Lonas'} 
-                  icon={<Cone/>}
                   onClick={()=> setViewAtiva('calculadoraLona')}
                 />
               </ListItem>
 
               <ListItem className={`p-0 w-full rounded-2xl ${viewAtiva === 'calculadoraEstrutura'? 'bg-purple-700': ''}`}>
+               
                 <BotaoMenu 
+                  icon={<ChevronRightIcon strokeWidth={3} className="h-3 w-5 self-center" />}
                   label={'Estruturas'} 
-                  icon={<Landmark/>}
                   onClick={()=> setViewAtiva('calculadoraEstrutura')}
                 />
               </ListItem>
@@ -74,12 +80,15 @@ export function SideBar() {
             icon={
               <ChevronDownIcon
                 strokeWidth={2.5}
-                className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
+                className={`mx-auto h-4 w-4 transition-transform ${open === 2 ? "rotate-180" : ""}`}
               />
             }
         >
           <ListItem className="p-0" selected={open === 2}>
-            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-2 hover:bg-card-escuro rounded-2xl">
+            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-2 hover:bg-card-escuro rounded-2xl gap-2">
+              <ListItemPrefix>
+                <Calculator className="w-5 h-5"/>
+              </ListItemPrefix>
               <Typography color="blue-gray" className="mr-auto font-normal text-lg">
                 Calculadoras
               </Typography>
@@ -91,7 +100,7 @@ export function SideBar() {
               <ListItem className={`p-0 w-full rounded-2xl ${viewAtiva === 'calculadoraCupula'? 'bg-purple-700': ''}`}>
                 <BotaoMenu 
                   label={'Cúpula'} 
-                  icon={<Pyramid/>}
+                  icon={<ChevronRightIcon strokeWidth={3} className="h-3 w-5 self-center" />} 
                   onClick={()=> setViewAtiva('calculadoraCupula')}
                 />
               </ListItem>
@@ -101,25 +110,39 @@ export function SideBar() {
         <hr className="my-2 border-fundo-verde" />
 
         <Accordion
-            open={open === 2}
+            open={open === 3}
             icon={
               <ChevronDownIcon
                 strokeWidth={2.5}
-                className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
+                className={`mx-auto h-4 w-4 transition-transform ${open === 3 ? "rotate-180" : ""}`}
               />
             }
         >
-          <ListItem className="p-0" selected={open === 3}>
-            <AccordionHeader onClick={() => handleOpen(3)} className="border-b-0 p-2 hover:bg-card-escuro rounded-2xl">
+          <ListItem className="p-0" selected={open === 2}>
+            <AccordionHeader onClick={() => handleOpen(3)} className="border-b-0 p-2 hover:bg-card-escuro rounded-2xl gap-2">
+              <ListItemPrefix>
+               <ClipboardPenLine  className="w-5 h-5"/>
+              </ListItemPrefix>
               <Typography color="blue-gray" className="mr-auto font-normal text-lg">
-                Planilha
+                Planilhas
               </Typography>
             </AccordionHeader>
           </ListItem>
 
-         
+          <AccordionBody className="py-1 overflow-hidden"> 
+            <List className="p-0 flex flex-col gap-2">
+              <ListItem className={`p-0 w-full rounded-2xl ${viewAtiva === 'calculadoraCupula'? 'bg-purple-700': ''}`}>
+                <BotaoPaginas 
+                  label={'Orçamento'} 
+                  to={'/impressao'}
+                  icon={<ChevronRightIcon strokeWidth={3} className="h-3 w-5 self-center" />} 
+                />
+              </ListItem>
+            </List>
+          </AccordionBody>
         </Accordion>
 
+       
       </List>
     </Card>
   );
